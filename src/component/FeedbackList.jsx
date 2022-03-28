e9ec67a272724b687f4e6b3a9be0185d7849546b
souravrane <sourav1121997@gmail.com>
2022-03-28 18:39:00 +0530

    Card

--- src/component/FeedbackList.jsx
+++ src/component/FeedbackList.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
+import PropTypes from "prop-types";
 
 function FeedbackList({ feedback }) {
-    if (!feedback || feedback.length == 0) {
+    if (!feedback || feedback.length === 0) {
         return <p>No Feedback Yet..</p>;
     }
 
@@ -14,5 +15,15 @@ function FeedbackList({ feedback }) {
         </div>
     );
 }
+
+FeedbackList.propTypes = {
+    feedback: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            rating: PropTypes.number.isRequired,
+        })
+    ),
+};
 
 export default FeedbackList;
